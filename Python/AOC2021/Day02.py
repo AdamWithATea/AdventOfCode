7740836b9251d668305a0ac7d225c5d682e4f2d8
Adam <95426758+AdamWithATea@users.noreply.github.com>
2023-06-15 01:00:28 +0100

    Rewrote all Python puzzles with shorter solutions

--- Python/AOC2021/Day02.py
+++ Python/AOC2021/Day02.py
@@ -6,43 +6,31 @@ def Run(useExamples):
     print('Day 2-2: ' + str(Part2(filepath)))
     
 def Part1(filepath):
-    directions = InputHandler.FileToStringList(filepath, '\n')
-    horizontal = 0
-    depth = 0
-
-    for instruction in directions:
-        #Split the full instruction into direction and distance
-        direction = instruction.split(' ')[0]
-        distance = instruction.split(' ')[1]
-        if direction == 'forward':
-            horizontal += int(distance)
-        elif direction == 'down':
-            depth += int(distance)
-        elif direction == 'up':
-            depth -= int(distance)
-
-    answer = horizontal * depth
-
-    return answer
+    return PlotCourse(InputHandler.FileToStringList(filepath, '\n'), False)
 
 def Part2(filepath):
-    directions = InputHandler.FileToStringList(filepath, '\n') 
+    return PlotCourse(InputHandler.FileToStringList(filepath, '\n'), True)
+
+def PlotCourse (directions, useAim):
     horizontal = 0
     depth = 0
-    aim = 0
-
+    aim = 0    
     for instruction in directions:
-        #Split the full instruction into direction and distance
         direction = instruction.split(' ')[0]
-        distance = instruction.split(' ')[1]
-        if direction == 'forward':
-            horizontal += int(distance)
-            depth += aim * int(distance)
-        elif direction == 'down':
-            aim += int(distance)
-        elif direction == 'up':
-            aim -= int(distance)
-
-    answer = horizontal * depth
-
-    return answer
+        distance = int(instruction.split(' ')[1])
+        match direction:
+            case "forward":
+                horizontal += distance
+                if (useAim):
+                    depth += aim * distance
+            case "down":
+                if (useAim):
+                    aim += distance
+                else:
+                    depth += distance
+            case "up":
+                if(useAim):
+                    aim -= distance
+                else:
+                    depth -= distance
+    return horizontal * depth
