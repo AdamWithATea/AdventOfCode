8f4c2b43033da402d2f4ea20f0b9a5c7c9491437
Adam <github@adamdenyer.com>
2023-12-08 14:16:43 +0000

    AOC 2023 initial setup

---
+++ CSharp/App/2023/Day01.cs
@@ -0,0 +1,11 @@
+using AdventOfCode;
+
+namespace AOC2023;
+public class Day01 : Day{
+    public override long Part1(string filepath){
+        return 0;
+    }
+    public override long Part2(string filepath){
+        return 0;
+    }
+}
---
+++ CSharp/App/2023/Year2023.cs
@@ -0,0 +1,10 @@
+using AdventOfCode;
+
+namespace AOC2023;
+public class Year2023 : Year{
+    public override List<Day> Days(){
+        return new List<Day>{
+            new Day01()
+        };
+    }
+}
