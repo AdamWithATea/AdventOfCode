2908ee301814c4892fe9aa3e5ccef34c5e56e97d
Adam <95426758+AdamWithATea@users.noreply.github.com>
2023-12-09 00:57:05 +0000

    C#: 2023 Day 1 completed

--- CSharp/Tests/Tests2023.cs
+++ CSharp/Tests/Tests2023.cs
@@ -7,9 +7,9 @@ public class Tests2023{
     [Fact]
     public void Day01Test(){
         Day01 day01 = new();
-        Assert.Equal(0, day01.Part1(Inputs.BuildFilepath(2023, 01, true)));
-        Assert.Equal(0, day01.Part1(Inputs.BuildFilepath(2023, 01, false)));
-        Assert.Equal(0, day01.Part2(Inputs.BuildFilepath(2023, 01, true)));
-        Assert.Equal(0, day01.Part2(Inputs.BuildFilepath(2023, 01, false)));
+        Assert.Equal(142, day01.Part1(Inputs.BuildFilepath(2023, 01, true)));
+        Assert.Equal(54968, day01.Part1(Inputs.BuildFilepath(2023, 01, false)));
+        Assert.Equal(281, day01.Part2(Inputs.BuildFilepath(2023, 01, true)));
+        Assert.Equal(54094, day01.Part2(Inputs.BuildFilepath(2023, 01, false)));
     }
 }
