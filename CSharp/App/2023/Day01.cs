a2d5c9b5eb6b75ef8e12c1435d044863f00cb5a2
Adam <github@adamdenyer.com>
2023-12-09 00:57:05 +0000

    C#: 2023 Day 1 completed

--- CSharp/App/2023/Day01.cs
+++ CSharp/App/2023/Day01.cs
@@ -1,11 +1,53 @@
+using System.Text.RegularExpressions;
 using AdventOfCode;
 
 namespace AOC2023;
 public class Day01 : Day{
     public override long Part1(string filepath){
-        return 0;
+        if (filepath.Contains("Examples")){
+            filepath = filepath[..^4] + "-Part1.txt";
+        }
+        List<string> inputs = Inputs.ListLines(filepath);
+        int calibrationSum = CalibrationSum(inputs, false);
+        return calibrationSum;
     }
     public override long Part2(string filepath){
-        return 0;
+        if (filepath.Contains("Examples")){
+            filepath = filepath[..^4] + "-Part2.txt";
+        }
+        List<string> inputs = Inputs.ListLines(filepath);
+        int calibrationSum = CalibrationSum(inputs, true);
+        return calibrationSum;
+    }
+    private static int CalibrationSum(List<string> inputs, bool includeWords){
+        int calibrationSum = 0;
+        foreach (string line in inputs){
+            string firstNumber = CalibrationDigit(line, false, includeWords);
+            string lastNumber = CalibrationDigit(line, true, includeWords);
+            _ = int.TryParse(firstNumber + lastNumber, out int calibrationValue);
+            calibrationSum += calibrationValue;
+        }
+        return calibrationSum;
+    }
+    private static string CalibrationDigit(string input, bool rightToLeft, bool includeWords){
+        string pattern = includeWords ? 
+            @"[1-9]|one|two|three|four|five|six|seven|eight|nine" : @"[1-9]";
+        RegexOptions options = rightToLeft ? RegexOptions.RightToLeft : RegexOptions.None;        
+        string number = Regex.Matches(input, pattern, options)[0].Value;
+        return NumericDigit(number);
+    }
+    private static string NumericDigit(string input){
+        return input switch{
+            "one" => "1",
+            "two" => "2",
+            "three" => "3",
+            "four" => "4",
+            "five" => "5",
+            "six" => "6",
+            "seven" => "7",
+            "eight" => "8",
+            "nine" => "9",
+            _ => input,
+        };
     }
 }
