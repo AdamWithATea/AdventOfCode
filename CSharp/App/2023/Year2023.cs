cdcedb8ef5debbcea113d07f880d52c93848a4e8
Adam <95426758+AdamWithATea@users.noreply.github.com>
2023-12-11 19:04:25 +0000

    C#: 2023 Day 2 completed

--- CSharp/App/2023/Year2023.cs
+++ CSharp/App/2023/Year2023.cs
@@ -4,7 +4,8 @@ namespace AOC2023;
 public class Year2023 : Year{
     public override List<Day> Days(){
         return new List<Day>{
-            new Day01()
+            new Day01(),
+            new Day02()
         };
     }
 }
--- CSharp/Tests/Tests2023.cs
+++ CSharp/Tests/Tests2023.cs
@@ -12,4 +12,12 @@ public class Tests2023{
         Assert.Equal(281, day01.Part2(Inputs.BuildFilepath(2023, 01, true)));
         Assert.Equal(54094, day01.Part2(Inputs.BuildFilepath(2023, 01, false)));
     }
+    [Fact]
+    public void Day02Test(){
+        Day02 day02 = new();
+        Assert.Equal(8, day02.Part1(Inputs.BuildFilepath(2023, 02, true)));
+        Assert.Equal(2771, day02.Part1(Inputs.BuildFilepath(2023, 02, false)));
+        Assert.Equal(2286, day02.Part2(Inputs.BuildFilepath(2023, 02, true)));
+        Assert.Equal(70924, day02.Part2(Inputs.BuildFilepath(2023, 02, false)));
+    }
 }
